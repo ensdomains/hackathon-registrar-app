da394bade8a962f0adee4e623b72bd03b111f80c
Jeff Lau <jeff-lau@live.com>
2019-03-07 13:09:56 +0800

    Change .env name

--- testing-utils/preTest.mjs
+++ testing-utils/preTest.mjs
@@ -1,6 +1,6 @@
-import deployTestEns from "./deployENS.js"
-import Web3 from "web3"
-import fs from "fs"
+import deployTestEns from './deployENS.js'
+import Web3 from 'web3'
+import fs from 'fs'
 
 let web3
 
@@ -14,7 +14,7 @@ async function setupWeb3(customProvider) {
 
   return {
     web3,
-    networkId,
+    networkId
   }
 }
 
@@ -22,16 +22,16 @@ async function init() {
   const ENV = process.argv[2]
 
   switch (ENV) {
-    case "GANACHE_GUI":
-      var provider = new Web3.providers.HttpProvider("http://localhost:7545")
+    case 'GANACHE_GUI':
+      var provider = new Web3.providers.HttpProvider('http://localhost:7545')
       var { web3 } = await setupWeb3(provider)
       break
-    case "GANACHE_CLI":
-      var provider = new Web3.providers.HttpProvider("http://localhost:8545")
+    case 'GANACHE_CLI':
+      var provider = new Web3.providers.HttpProvider('http://localhost:8545')
       var { web3 } = await setupWeb3(provider)
       break
     default:
-      var provider = new Web3.providers.HttpProvider("http://localhost:8545")
+      var provider = new Web3.providers.HttpProvider('http://localhost:8545')
       var { web3 } = await setupWeb3(provider)
       break
   }
@@ -41,15 +41,11 @@ async function init() {
   const addresses = await deployTestEns({ web3, accounts })
   const ensAddress = addresses.ensAddress
 
-  fs.writeFileSync("./cypress.env.json", JSON.stringify(addresses))
-  fs.writeFile(
-    "./.env.development",
-    `GATSBY_ENS_ADDRESS=${ensAddress}`,
-    err => {
-      if (err) throw err
-      console.log("Wrote address " + ensAddress + " to .env.local")
-    }
-  )
+  fs.writeFileSync('./cypress.env.json', JSON.stringify(addresses))
+  fs.writeFile('./.env.local', `REACT_APP_ENS_ADDRESS=${ensAddress}`, err => {
+    if (err) throw err
+    console.log('Wrote address ' + ensAddress + ' to .env.local')
+  })
 }
 
 init()
