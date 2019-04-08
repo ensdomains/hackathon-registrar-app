2464e5cd05b5b0e4515c409516de20a835a9e7de
Jeff Lau <jeff-lau@live.com>
2019-04-08 12:02:02 +0800

    Fix

--- src/components/library/useENS.js
+++ src/components/library/useENS.js
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react'
-import getENS, { getHackathonRegistrar } from './ens'
+import getENS, { getHackathonRegistrar, getNamehash } from './ens'
 
-export default function useENS(web3Loading) {
+export default function useENS(web3Loading, domain) {
   const [loading, setLoading] = useState(true)
   const [ENS, setENS] = useState(undefined)
   const [readENS, setReadENS] = useState(undefined)
@@ -9,11 +9,16 @@ export default function useENS(web3Loading) {
 
   const fetchENS = async (web3Loading, domain) => {
     if (web3Loading) return
+    console.log(1)
     const { ENS, readENS } = await getENS()
-    const hackathonRegisaddresstrarAddress = ENS.owner(domain).call()
-    const { registrar } = await getHackathonRegistrar(
-      hackathonRegisaddresstrarAddress
-    )
+    console.log(2)
+    console.log(domain)
+    const hackathonRegistrarAddress = await readENS
+      .owner(await getNamehash(domain))
+      .call()
+    console.log(3)
+    console.log(hackathonRegistrarAddress)
+    const { registrar } = await getHackathonRegistrar(hackathonRegistrarAddress)
 
     setENS(ENS)
     setReadENS(readENS)
@@ -22,7 +27,7 @@ export default function useENS(web3Loading) {
   }
 
   useEffect(() => {
-    fetchENS(web3Loading)
+    fetchENS(web3Loading, domain)
   }, [web3Loading])
 
   return { ENS, readENS, hackathonRegistrar: registrar, loading }
